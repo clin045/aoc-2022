a6632c7d86b3abbb78127294fbaa7b671812e477
Christopher Lin <christopher.lin045@gmail.com>
2022-12-17 13:10:07 -0500

    Day 8, unfinished day 7

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -3,10 +3,96 @@ use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
+struct DirNode {
+    idx: usize,
+    name: String,
+    filesize: u64,
+    children: Vec<usize>,
+    parent: Option<usize>
+}
+
+struct FileTree{
+    tree: Vec<DirNode>
+}
+impl FileTree{
+    fn new() -> FileTree {
+        FileTree{tree: Vec::<DirNode>::new()}
+    }
+    fn create_node(&mut self, parent: usize, name: String) -> usize {
+        let new_idx = self.tree.len();
+        let new_node = DirNode{
+            idx: new_idx,
+            name: name,
+            filesize: 0,
+            children: Vec::<usize>::new(),
+            parent: None
+        };
+        self.tree.push(new_node);
+        let parent_node = self.tree.get_mut(parent).unwrap();
+        parent_node.children.push(new_idx);
+        new_idx
+    }
+}
+
 pub fn solve() -> SolutionPair {
+    println!("{:?}", read_input());
     // Your solution here...
     let sol1: u64 = 0;
     let sol2: u64 = 0;
 
     (Solution::U64(sol1), Solution::U64(sol2))
+}
+
+fn read_input() -> Vec<String> {
+    let raw_input = read_to_string("input/07_test.txt").unwrap();
+    let line_split = raw_input.split("\n").map(|x| x.to_owned()).collect::<Vec<String>>();
+    line_split
+}
+
+fn build_filetree(commands: Vec<String>) -> FileTree{
+    let mut fs_tree = FileTree::new();
+    let mut current: usize = 0;
+    for cmd in commands {
+        let cmd_split: Vec<&str> = cmd.split_whitespace().collect();
+        match cmd_split[1] {
+            "cd" => {
+                let dst = cmd_split[2];
+                match dst {
+                    "/" => {
+                        current = 0;
+                    },
+                    ".." => {
+                        let parent_idx = fs_tree.tree.get(current).unwrap().parent.unwrap();
+                        current = parent_idx;
+                    }
+                    _ => {
+                        let child_names: Vec<(String, usize)> = fs_tree.tree.get(current).unwrap()
+                            .children
+                            .iter()
+                            .map(|child| {
+                                let name = fs_tree.tree.get(*child).unwrap().name.to_owned();
+                                (name, *child) 
+                            }).collect();
+                        current = child_names.iter().find(|x| {
+                            x.0 == dst
+                        }).unwrap().1;
+                    }
+                }
+            },
+            "ls" => {},
+            _ => {
+                match cmd_split[0] {
+                    "dir" => {
+                        fs_tree.create_node(current, cmd_split[1].to_owned());
+                    },
+                    _ => {
+                        let filesize = cmd_split[0].parse::<u64>().unwrap();
+                        let cur_node = fs_tree.tree.get_mut(current).unwrap();
+                        cur_node.filesize += filesize;
+                    }
+                }
+            }
+        }
+    }
+    todo!();
 }
--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,12 +1,101 @@
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::{fs::read_to_string};
+use ndarray::{Array2};
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
+enum Axis {
+    I,
+    J
+}
+
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = read_input();
+    let sol1: u64 = input.indexed_iter().map(
+        |x| {
+            seeable(&input, x.0, Axis::I, 1) ||
+            seeable(&input, x.0, Axis::I, -1) ||
+            seeable(&input, x.0, Axis::J, 1) ||
+            seeable(&input, x.0, Axis::J, -1)
+        }
+    ).map(|x| x as u64).sum();
+
+    // let sol1: u64 = tmp.iter().sum();
+    let sol2:u64 = input.indexed_iter().map(
+        |x| {
+            seeable2(&input, x.0, Axis::I, 1) *
+            seeable2(&input, x.0, Axis::I, -1) *
+            seeable2(&input, x.0, Axis::J, 1) *
+            seeable2(&input, x.0, Axis::J, -1)
+        }
+    ).max().unwrap();
 
     (Solution::U64(sol1), Solution::U64(sol2))
+}
+
+fn read_input() -> Array2<u64> {
+    
+    let raw_input = read_to_string("input/08.txt").unwrap();
+    let line_split: Vec<_> = raw_input.split("\n").collect();
+    let i_len = line_split.len();
+    let j_len = line_split[0].len();
+    let mut trees: Array2<u64> = Array2::zeros((i_len, j_len));
+    for (i, _) in line_split.iter().enumerate() {
+        for (j, char) in line_split[i].chars().enumerate(){
+            trees[[i, j]] = char.to_digit(10).unwrap() as u64;
+        }
+    }
+
+    trees
+}
+
+fn seeable(trees: &Array2<u64>, start: (usize, usize), axis: Axis, step: i32) -> bool {
+    let start_val = trees[[start.0, start.1]];
+    match axis {
+        Axis::I => {
+            let mut i_counter = start.0 as i32 + step;
+            while let Some(checkval) = trees.get([i_counter as usize, start.1]){
+                if *checkval >= start_val {return false}
+                i_counter += step;
+            }
+            
+        },
+        Axis::J => {
+            let mut j_counter = start.1 as i32 + step;
+            while let Some(checkval) = trees.get([start.0, j_counter as usize]){
+                if *checkval >= start_val {return false}
+                j_counter += step;
+            }
+        }
+    }
+    true
+}
+
+fn seeable2(trees: &Array2<u64>, start: (usize, usize), axis: Axis, step: i32) -> u64 {
+    let start_val = trees[[start.0, start.1]];
+    match axis {
+        Axis::I => {
+            let mut seen: u64 = 0;
+            let mut i_counter = start.0 as i32 + step;
+            while let Some(checkval) = trees.get([i_counter as usize, start.1]){
+                seen += 1;
+                if *checkval >= start_val {break}
+                i_counter += step;
+            }
+            return seen
+            
+        },
+        Axis::J => {
+            let mut j_counter = start.1 as i32 + step;
+            let mut seen: u64 = 0;
+            while let Some(checkval) = trees.get([start.0, j_counter as usize]){
+                seen += 1;
+                if *checkval >= start_val {break}
+                j_counter += step;
+            }
+            return seen
+        }
+    }
 }
